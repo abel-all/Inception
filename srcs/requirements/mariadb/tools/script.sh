ff8e08379a8ca74c9e87d536e388b40a80f26aa9
Abdessamad El Allali <abel-all@e2r1p5.1337.ma>
2024-02-02 10:28:54 +0100

    complete mariadb container

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,20 +1,17 @@
+#!/bin/sh
 
+db_name=wordpressdb
+db_user=abelwb
+db_pwd=Abdessamad010203#
 
-# db_name=wordpressdb
-# db_user=abelwb
-# db_pwd=Abdessamad010203#
-
-# # Make sure that NOBODY can access the server without a password
-# mysql -e "CREATE DATABASE IF NOT EXISTS $db_name ;"
-# # Kill the anonymous users
-# mysql -e "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd' ;"
-# # Because our hostname varies we'll use some Bash magic here.
-# mysql -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%' ;"
-# # Kill off the demo database
-# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;"
-# # Make our changes take effect
-# mysql -e "FLUSH PRIVILEGES;"
-# # Any subsequent tries to run queries this way will get access denied because lack of usr/pwd param
-CREATE DATABASE IF NOT EXISTS wordpress;
-GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress_user'@'%' IDENTIFIED BY 'wordpress_password';
-FLUSH PRIVILEGES;
+# Make sure that NOBODY can access the server without a password
+mysql -e "CREATE DATABASE IF NOT EXISTS $db_name ;"
+# Kill the anonymous users
+mysql -e "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd' ;"
+# Because our hostname varies we'll use some Bash magic here.
+mysql -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%' ;"
+# Kill off the demo database
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;"
+# Make our changes take effect
+mysql -e "FLUSH PRIVILEGES;"
+# Any subsequent tries to run queries this way will get access denied because lack of usr/pwd param
