ab8bafc133dd855fdf161a4784757a181bc0f59d
Abdessamad El Allali <abel-all@e2r1p5.1337.ma>
2024-02-03 17:27:37 +0100

    complete wd container

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -63,7 +63,6 @@ wp theme install astra --activate --allow-root
 
 # wp plugin install redis-cache --activate --allow-root
 
-
 # uses the sed command to modify the www.conf file in the /etc/php/7.3/fpm/pool.d directory. The s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g command substitutes the value 9000 for /run/php/php7.3-fpm.sock throughout the file. This changes the socket that PHP-FPM listens on from a Unix domain socket to a TCP port.
 sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g' /etc/php/7.4/fpm/pool.d/www.conf
 
