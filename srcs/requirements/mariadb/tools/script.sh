5eb8295c3cd1cacdcecbe1d368214a437fc6a1c4
Abdessamad El Allali <abel-all@e2r1p5.1337.ma>
2024-02-04 12:20:26 +0100

    complete wd container 2

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -11,14 +11,14 @@ sleep 10
 # mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;"
 # Make sure that NOBODY can access the server without a password
 echo "root password changed successfully"
-mysql -p12345 -e "CREATE DATABASE IF NOT EXISTS $db_name ;"
+mysql -e "CREATE DATABASE IF NOT EXISTS $db_name ;"
 echo "database created successfully"
 # Kill the anonymous users
-mysql -p12345 -e "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd' ;"
+mysql -e "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd' ;"
 # Because our hostname varies we'll use some Bash magic here.
-mysql -p12345 -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%' ;"
+mysql -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%' ;"
 # Make our changes take effect
-mysql -p12345 -e "FLUSH PRIVILEGES;"
+mysql -e "FLUSH PRIVILEGES;"
 # Any subsequent tries to run queries this way will get access denied because lack of usr/pwd param
 
 sleep infinity
