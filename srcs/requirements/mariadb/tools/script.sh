070ec9f6b039ae86c39dcfeae6eb278536986690
Abdessamad El Allali <abel-all@e2r1p5.1337.ma>
2024-02-04 12:43:28 +0100

    complete wd container 3

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -8,15 +8,15 @@ mysqld_safe
 
 sleep 15
 # Kill off the demo database
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;"
+mysql -e "ALTER USER root@'localhost' IDENTIFIED BY '12345' ;"
 # Make sure that NOBODY can access the server without a password
 echo "root password changed successfully"
 mysql -p 12345 -u root -e "CREATE DATABASE IF NOT EXISTS $db_name ;"
 echo "database created successfully"
 # Kill the anonymous users
-mysql -p 12345 -u root -e "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd' ;"
+mysql -p 12345 -u root -e "CREATE USER IF NOT EXISTS $db_user@'%' IDENTIFIED BY '$db_pwd' ;"
 # Because our hostname varies we'll use some Bash magic here.
-mysql -p 12345 -u root -e "GRANT ALL PRIVILEGES ON *.* TO '$db_user'@'%' ;"
+mysql -p 12345 -u root -e "GRANT ALL PRIVILEGES ON *.* TO $db_user@'%' ;"
 # Make our changes take effect
 mysql -p 12345 -u root -e "FLUSH PRIVILEGES;"
 # Any subsequent tries to run queries this way will get access denied because lack of usr/pwd param
