8d1f6b99a496c54167b3af374bc3039d8f357972
Abdessamad El Allali <abel-all@e2r1p5.1337.ma>
2024-02-04 22:58:22 +0100

    complete wd container 3

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -7,7 +7,7 @@ db_pwd=a123
 # mysqld_safe --skip-grant-tables --skip-syslog --skip-networking
 mysqld_safe
 
-sleep 10
+# sleep 10
 # Kill off the demo database
 mysql -e "ALTER USER root@'localhost' IDENTIFIED BY '12345' ;"
 # Make sure that NOBODY can access the server without a password
@@ -20,6 +20,6 @@ mysql -p 12345 -u root -e "CREATE USER IF NOT EXISTS $db_user@'%' IDENTIFIED BY 
 mysql -p 12345 -u root -e "GRANT ALL PRIVILEGES ON *.* TO $db_user@'%' ;"
 # Make our changes take effect
 mysql -p 12345 -u root -e "FLUSH PRIVILEGES;"
-# Any subsequent tries to run queries this way will get access denied because lack of usr/pwd param
+# Any subsequentt tries to run queries this way will get access denied because lack of usr/pwd param
 
 sleep infinity
