c678da00f1b1d3ac3f462722f35050b4a1cbbccb
Abdessamad El Allali <abel-all@e2r1p5.1337.ma>
2024-02-04 23:16:27 +0100

    complete wd container 3

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -20,7 +20,7 @@ mysql -e "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd' ;"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;"
 # Because our hostname varies we'll use some Bash magic here.
 echo "user on created successfully"
-mysql -p12345 -e "GRANT ALL PRIVILEGES ON *.* TO '$db_user'@'%' ;"
+mysql -u root -p12345 -e "GRANT ALL PRIVILEGES ON *.* TO '$db_user'@'%' ;"
 echo "user created successfully"
 # Make our changes take effect
 mysql -p12345 -e "FLUSH PRIVILEGES;"
