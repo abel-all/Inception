94935038848e660639fc24aa84188009620ccfd6
Abdessamad El Allali <abel-all@e2r1p5.1337.ma>
2024-02-04 22:51:24 +0100

    globurl=noglob urlglobber

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -2,16 +2,17 @@
 
 db_name=wordpressdb
 db_user=abelwb
-db_pwd=Abdessamad010203@
+db_pwd=a123
 
-mysqld_safe --skip-grant-tables --skip-syslog --skip-networking
+# mysqld_safe --skip-grant-tables --skip-syslog --skip-networking
+mysqld_safe
 
 sleep 10
 # Kill off the demo database
 mysql -e "ALTER USER root@'localhost' IDENTIFIED BY '12345' ;"
 # Make sure that NOBODY can access the server without a password
 echo "root password changed successfully"
-mysql -p 12345 -u root -e "CREATE DATABASE IF NOT EXISTS $db_name ;"
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS $db_name ;"
 echo "database created successfully"
 # Kill the anonymous users
 mysql -p 12345 -u root -e "CREATE USER IF NOT EXISTS $db_user@'%' IDENTIFIED BY '$db_pwd' ;"
