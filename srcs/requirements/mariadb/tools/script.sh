c4b43f523f259a64509348a7f8219ce7281d55d2
Abdessamad El Allali <abel-all@e2r1p5.1337.ma>
2024-02-05 10:15:20 +0100

    nvim=/Users/abel-all/.brew/bin:/Users/abel-all/.brew/bin:/Users/abel-all/.brew/bin:/Users/abel-all/.brew/bin:/Users/abel-all/.brew/bin:/Users/abel-all/.brew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/munki:/Library/Apple/usr/bin:/Users/abel-all/nvim-macos/bin:/Users/abel-all/bin/rg_dir:/Users/abel-all/bin/fd_dir:/Users/abel-all/nvim-macos/bin:/Users/abel-all/nvim-macos/bin:/Users/abel-all/nvim-macos/bin:/Users/abel-all/bin/rg_dir:/Users/abel-all/bin/fd_dir:/Users/abel-all/nvim-macos/bin:/Users/abel-all/bin/rg_dir:/Users/abel-all/bin/fd_dir:/Users/abel-all/nvim-macos/bin:/Users/abel-all/bin/rg_dir:/Users/abel-all/bin/fd_dir:/Users/abel-all/nvim-macos/bin:/Users/abel-all/bin/rg_dir:/Users/abel-all/bin/fd_dir:/Users/abel-all/nvim-macos/bin:/Users/abel-all/bin/rg_dir:/Users/abel-all/bin/fd_dir:/Users/abel-all/nvim-macos/bin/nvim

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -4,27 +4,22 @@ db_name=wordpressdb
 db_user=abelwb
 db_pwd=a123
 
-# mysqld_safe --skip-grant-tables --skip-syslog --skip-networking
-# mysqld_safe
-service mariadb start
 # mariadb start
-echo "hello"
-# sleep 10
-# Kill off the demo database
-echo "root password changed successfully"
-mysql -e "CREATE DATABASE IF NOT EXISTS $db_name ;"
-echo "database created successfully"
-# Kill the anonymous users
-mysql -e "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd' ;"
-# Make sure that NOBODY can access the server without a password
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;"
-# Because our hostname varies we'll use some Bash magic here.
-echo "user on created successfully"
-mysql -u root -p12345 -e "GRANT ALL PRIVILEGES ON *.* TO '$db_user'@'%' ;"
-echo "user created successfully"
-# Make our changes take effect
-mysql -p12345 -e "FLUSH PRIVILEGES;"
-# Any subsequentt tries to run queries this way will get access denied because lack of usr/pwd param
-echo "dcdfdfdfdfdf"
+service mariadb start
 
-sleep infinity
+# Make sure that NOBODY can access the server without a password
+mariadb -e "UPDATE mysql.user SET Password = PASSWORD('1234') WHERE User = 'root'"
+
+# And then we need to kill the anonymous users
+mariadb -e "DROP USER ''@'localhost'"
+
+# Because our hostname varies we'll use some Bash magic here.
+mariadb -e "DROP USER ''@'$(hostname)'"
+
+# Kill off the demo database
+mariadb -e "DROP DATABASE test"
+
+# Make our changes take effect
+mariadb -e "FLUSH PRIVILEGES"
+
+# sleep infinity
