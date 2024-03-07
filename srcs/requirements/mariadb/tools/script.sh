85c3de3b1eebff57c3c0e54d6ee6eed79f220c08
abel-all <abel-all@gmail.com>
2024-03-07 13:44:40 +0100

    hjsdg

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-mysql_install_db --user=mysql --ldata=/var/lib/mysql
+# mysql_install_db --user=mysql --ldata=/var/lib/mysql
 
 service mariadb start
 
-# Make sure that NOBODY can access the server without a password
 mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '123';"
-# sudo mariadbd --user=root --password=123
 
 mariadb -h localhost -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS wpdb;"
 
 mariadb -h localhost -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER 'abel-all'@'%' IDENTIFIED BY '123'; GRANT ALL PRIVILEGES ON wpdb.* TO 'abel-all'@'%'; FLUSH PRIVILEGES;"
 
-# service mariadb stop
 mariadb-admin -p$MYSQL_ROOT_PASSWORD shutdown
 
 mariadbd
-# sleep infinity
-# exec "$@"
